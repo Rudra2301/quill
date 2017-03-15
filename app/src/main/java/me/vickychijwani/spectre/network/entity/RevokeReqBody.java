ebbb8b619d659c1eff7c6988fcd9ba6c448799b6
Vicky Chijwani <vickychijwani@gmail.com>
2017-03-16 03:16:55 +0530

    Move token revocation to AuthService component.

--- app/src/main/java/me/vickychijwani/spectre/network/entity/RevokeReqBody.java
+++ app/src/main/java/me/vickychijwani/spectre/network/entity/RevokeReqBody.java
@@ -6,11 +6,11 @@ import android.support.annotation.StringDef;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@SuppressWarnings({"WeakerAccess", "unused"})
+@SuppressWarnings({"WeakerAccess"})
 public class RevokeReqBody {
 
-    public static final String TOKEN_TYPE_ACCESS = "access_token";
-    public static final String TOKEN_TYPE_REFRESH = "refresh_token";
+    private static final String TOKEN_TYPE_ACCESS = "access_token";
+    private static final String TOKEN_TYPE_REFRESH = "refresh_token";
 
     @StringDef({TOKEN_TYPE_ACCESS, TOKEN_TYPE_REFRESH})
     @Retention(RetentionPolicy.SOURCE)
@@ -22,11 +22,19 @@ public class RevokeReqBody {
     public final String clientId;
     @Nullable public final String clientSecret;
 
-    public RevokeReqBody(@TokenType String tokenTypeHint, String token, @Nullable String clientSecret) {
+    private RevokeReqBody(@TokenType String tokenTypeHint, String token, @Nullable String clientSecret) {
         this.tokenTypeHint = tokenTypeHint;
         this.token = token;
         this.clientId = "ghost-admin";
         this.clientSecret = clientSecret;
+    }
+
+    public static RevokeReqBody fromAccessToken(String token, String clientSecret) {
+        return new RevokeReqBody(TOKEN_TYPE_ACCESS, token, clientSecret);
+    }
+
+    public static RevokeReqBody fromRefreshToken(String token, String clientSecret) {
+        return new RevokeReqBody(TOKEN_TYPE_REFRESH, token, clientSecret);
     }
 
 }
